84add3aef832a686ac59fa9f8e28802b43dc2b4c
Peter Byfield <byfield554@gmail.com>
2019-02-17 10:16:19 +0000

    More validation

--- src/web-api/src/auth/dtos/user-register.dto.ts
+++ src/web-api/src/auth/dtos/user-register.dto.ts
@@ -1,5 +1,19 @@
+import { IsString, Length, Matches } from 'class-validator';
+
 export class UserRegisterDto {
+    @IsString()
+    @Length(5)
     username: string;
+
+    @IsString()
+    @Length(8)
+    @Matches(/\d/, {
+        message: 'Password must contain at least one digit.',
+    })
+    @Matches(/[^\w]/, {
+        message: 'Password must contain at least one special character.',
+    })
     password: string;
+
     confirmPassword: string;
 }
