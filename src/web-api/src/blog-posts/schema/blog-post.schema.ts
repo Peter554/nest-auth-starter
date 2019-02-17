e026a3fdd7c89c491427e3da08b508e72c7cfaf5
Peter Byfield <byfield554@gmail.com>
2019-02-17 09:48:22 +0000

    Blog posts have an author

--- src/web-api/src/blog-posts/schema/blog-post.schema.ts
+++ src/web-api/src/blog-posts/schema/blog-post.schema.ts
@@ -3,9 +3,11 @@ import { Document, Schema } from 'mongoose';
 export interface IBlogPost extends Document {
   subject: string;
   body: string;
+  authorId: string;
 }
 
 export const BlogPostSchema = new Schema({
   subject: String,
   body: String,
+  authorId: String,
 });
